29762ba320cec275fde8b37bc053e1375b67c6f5
frgiovanna <giovannacnf@gmail.com>
2024-11-29 15:25:15 -0300

    code improvements

--- src/shared/components/Card/styles.ts
+++ src/shared/components/Card/styles.ts
@@ -1,7 +1,6 @@
 import styled from '@emotion/styled';
 import { StyledCardProps } from './types';
 import { theme } from '../../theme';
-import { css } from '@emotion/react';
 
 export const StyledCard = styled.section<StyledCardProps>`
   padding: 72px 32px;
@@ -9,12 +8,6 @@ export const StyledCard = styled.section<StyledCardProps>`
   background: ${theme.colors.background.light};
   box-shadow: ${theme.shadow};
 
-  ${({ position }) =>
-    position === 'bottom'
-      ? css`
-          border-radius: 40px 40px 0px 0px;
-        `
-      : css`
-          border-radius: 0px 0px 40px 40px;
-        `}
+  border-radius: ${({ position }) =>
+    position === 'bottom' ? '40px 40px 0px 0px' : '0px 0px 40px 40px;'};
 `;
--- src/shared/components/Typography/styles.ts
+++ src/shared/components/Typography/styles.ts
@@ -2,34 +2,35 @@ import styled from '@emotion/styled';
 import { StyledTypographyProps } from './types';
 import { css } from '@emotion/react';
 
+function getPropertiesFromExternal(
+  color: StyledTypographyProps['color'],
+  textAlign: StyledTypographyProps['textAlign'],
+) {
+  return css`
+    color: ${color};
+    text-align: ${textAlign};
+  `;
+}
+
 export const H1 = styled.h1<StyledTypographyProps>`
   font-size: 40px;
   font-weight: 600;
   margin: 0;
-  ${({ color, textAlign }) => css`
-    color: ${color};
-    text-align: ${textAlign};
-  `}
+  ${({ color, textAlign }) => getPropertiesFromExternal(color, textAlign)}
 `;
 
 export const H2 = styled.h2<StyledTypographyProps>`
   font-size: 32px;
   font-weight: 600;
   margin: 0;
-  ${({ color, textAlign }) => css`
-    color: ${color};
-    text-align: ${textAlign};
-  `}
+  ${({ color, textAlign }) => getPropertiesFromExternal(color, textAlign)}
 `;
 
 export const H3 = styled.h3<StyledTypographyProps>`
   font-size: 24px;
   font-weight: 600;
   margin: 0;
-  ${({ color, textAlign }) => css`
-    color: ${color};
-    text-align: ${textAlign};
-  `}
+  ${({ color, textAlign }) => getPropertiesFromExternal(color, textAlign)}
 `;
 
 export const Body2 = styled.p<StyledTypographyProps>`
@@ -37,10 +38,7 @@ export const Body2 = styled.p<StyledTypographyProps>`
   font-size: 14px;
   line-height: 20px;
   margin: 0;
-  ${({ color, textAlign }) => css`
-    color: ${color};
-    text-align: ${textAlign};
-  `}
+  ${({ color, textAlign }) => getPropertiesFromExternal(color, textAlign)}
 `;
 
 export const Body1 = styled.p<StyledTypographyProps>`
@@ -48,8 +46,5 @@ export const Body1 = styled.p<StyledTypographyProps>`
   font-size: 16px;
   line-height: 20px;
   margin: 0;
-  ${({ color, textAlign }) => css`
-    color: ${color};
-    text-align: ${textAlign};
-  `}
+  ${({ color, textAlign }) => getPropertiesFromExternal(color, textAlign)}
 `;
