027d392ad7ad5df87b6ff07737625109440d0ffc
Giovanna Freitas <54802614+frgiovanna@users.noreply.github.com>
2024-12-04 10:10:58 -0300

    add leave queue action (#26)

--- src/pages/Lobby/Lobby.tsx
+++ src/pages/Lobby/Lobby.tsx
@@ -1,7 +1,8 @@
-import { Typography } from '../../shared/components/Typography';
-import { theme } from '../../shared/theme';
+import { Button } from '@shared/components/Button';
+import { Typography } from '@shared/components/Typography';
+import { theme } from '@shared/theme';
 import { PlayersTable } from './components/PlayersTable';
-import { Container, Content } from './styles';
+import { Container, Content, TableWrapper } from './styles';
 
 export function Lobby() {
   const players = [
@@ -13,11 +14,15 @@ export function Lobby() {
     <Container>
       {/* Court Card */}
       <Content>
-        <Typography variant="H3" color={theme.colors.text.heading.dark}>
-          Em espera
-        </Typography>
-        <PlayersTable players={players} />
-        {/* Leave Queue Action */}
+        <TableWrapper>
+          <Typography variant="H3" color={theme.colors.text.heading.dark}>
+            Em espera
+          </Typography>
+          {/* Lobby description */}
+
+          <PlayersTable players={players} />
+        </TableWrapper>
+        <Button label="Sair da fila" onClick={() => {}} variant="text" />
       </Content>
     </Container>
   );
